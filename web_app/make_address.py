08eda9b17e224eafa5b3c06abba13b3f8affa431
npgeorge <17250377+pcsubirachs@users.noreply.github.com>
2021-05-25 16:38:20 -0700

    fix

--- web_app/make_address.py
+++ web_app/make_address.py
@@ -44,6 +44,65 @@ multi_sig = mk_multisig_script(pub1, pub2, pub3, 2, 3)
 # need to convert script to hash, P2SH or Pay-to-Script Hash
 multi_addr = scriptaddr(multi_sig)
 print("Multi Sig Address :", multi_addr)
+
+
+# Pybit
+
+## An open-source python Flask app to generate simple, multi-sig, and brain phrase Bitcoin wallets
+
+### Home Page
+
+<img src="https://github.com/pcsubirachs/pybit_wallet/media/home_page.jpeg " alt="drawing" align="middle" width="800"/>
+
+### Creating a simple Bitcoin wallet
+
+<img src="https://github.com/pcsubirachs/pybit_wallet/media/simple.jpeg" alt="drawing" align="middle" width="800"/>
+
+### Create a K of N multi-sig wallet
+
+<img src="https://github.com/pcsubirachs/pybit_wallet/media/K_of_N.jpeg" alt="drawing" align="middle" width="800"/>
+
+### Create a brain phrase wallet
+
+<img src="https://github.com/pcsubirachs/pybit_wallet/media/brain_phrase.jpeg" alt="drawing" align="middle" width="800"/>
+
+## Link to App
+[PyBit: An open-source Bitcoin wallet generator](https://github.com/pcsubirachs/pybit_wallet "PyBit")
+
+## How it Works
+
+Use this app to generate simple original, multi-sig or brain phrase Bitcoin wallets.
+
+### Simple Wallet
+
+Description here...
+
+### Multi-Sig Wallet
+
+Description here...
+
+## Brain Phrase
+
+Description here....
+
+# Developer: Setup & Install
+
+1. Fork repo
+
+2. pip install pipenv
+
+3. pipenv shell
+
+4. pipenv install pip
+
+5. export FLASK_APP=web_app
+
+6. export FLASK_ENV=developer
+
+7. flask run
+
+Should now be running on http://127.0.0.1:5000/
+
 """
 
 def make_address():
